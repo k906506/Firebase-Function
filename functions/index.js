439974eb94fe2992a77f9cf07005473709634908
k906506 <k906506@naver.com>
2023-04-03 18:26:06 +0900

    feat: FCM Trigger 메서드 구현

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.sendNotification = functions.pubsub
+    .schedule("every 5 minutes")
+    .onRun(async (context) => {
+      const querySnapshot = await admin
+          .firestore()
+          .collection("User")
+          .get();
+
+      querySnapshot.forEach(async (doc) => {
+        const userId = doc.id;
+        const token = doc.data().deviceToken;
+
+        const message = {
+          token: token,
+          notification: {
+            title: "Notification Title",
+            body: "Notification Body",
+          },
+          data: {
+            click_action: "NOTIFICATION_CLICK",
+            screen: "HOME_SCREEN",
+          },
+        };
+
+        const response = await admin.messaging().send(message);
+        console.log(`Notification sent to ${userId} with response:`, response);
+      });
+    });
